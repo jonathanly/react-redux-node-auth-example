0914c84deb5ce36367a0af2d82c5986bb27ad17f
Jonathan Ly <jonathanlyhv@gmail.com>
2017-05-23 23:43:33 +1000

    Fixed bug not sending JWT to client on user sign up

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -23,7 +23,7 @@ exports.signup = function(req, res, next) {
 
   User.findOne({ email }, function(err, existingUser) {
     if (err) { return next(err); }
-    console.log(existingUser)
+
     if (existingUser) {
       return res.status(422).send({ error: 'Email is in use' });
     }
@@ -35,7 +35,7 @@ exports.signup = function(req, res, next) {
 
     user.save()
       .then(newUser => {
-        res.json(user);
+        res.send({ token: tokenForUser(user) });
       })
       .catch(err => {
         return next(err)
