b2f957a61a8479514dafb4562d909002081bdc91
Jonathan Ly <jonathanlyhv@gmail.com>
2017-05-23 23:40:54 +1000

    Defined app routing

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default class App extends Component {
+import Header from './Header';
+import Feature from './Feature';
+import SignIn from './Auth/SignIn';
+import SignUp from './Auth/SignUp';
+import SignOut from './Auth/SignOut';
+import RequireAuth from './Auth/RequireAuth';
+
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        <Switch>
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/signout' component={SignOut} />
+          <Route path='/feature' component={RequireAuth(Feature)} />
+        </Switch>
+      </div>
     );
   }
 }
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
-import App from './components/app';
+import App from './components/App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container')
+);
